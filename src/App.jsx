128e25cb0346388ccc1a3f698098f795a1546b12
Jason Smith <smija13@gmail.com>
2024-08-12 00:38:03 -0700

    Add children to UserCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
 import './App.css'
+import UserCard from './components/UserCard'
+import UserInfo from './components/UserInfo'
+import SocialLinkList from './components/SocialLinkList'
+import UserImage from './assets/ProfilePicSquare.jpg'
 
 function App() {
   return (
-    <>
-    </>
+    <UserCard>
+      <UserInfo
+        userImage={UserImage}
+        name="Jason Smith"
+        location="Fresno, CA"
+        quote={`"Front-End Developer and Gamer"`}
+      />
+      <SocialLinkList />
+    </UserCard>
   )
 }
 
